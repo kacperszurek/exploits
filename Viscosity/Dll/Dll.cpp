d6959f66f06d6ed3bdd190d075d49a2cec191136
Kacper Szurek <kacperszurek@gmail.com>
2017-01-31 13:45:35 +0100

    Viscosity for Windows 1.6.7 Privilege Escalation

---
+++ Viscosity/Dll/Dll.cpp
@@ -0,0 +1,6 @@
+// Dll.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+
---
+++ Viscosity/Dll/stdafx.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
